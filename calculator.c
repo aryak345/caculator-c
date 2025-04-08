37a61dfb4be30f6b85f4c377fff744fc44f6f2e2
Arya <arya.kulkarni.345@gmail.com>
2025-04-08 22:26:50 +0530

    Added mod operator

--- calculator.c
+++ calculator.c
@@ -13,19 +13,23 @@ int main() {
     scanf("%lf", &b);
 
     switch(op) {
-        case '+': printf("Result: %.2lf\n", a + b); break;
-        case '-': printf("Result: %.2lf\n", a - b); break;
-        case '*': printf("Result: %.2lf\n", a * b); break;
-        case '/': 
-            if (b != 0)
-                printf("Result: %.2lf\n", a / b);
-            else
-                printf("Cannot divide by zero.\n");
-            break;
-        case '^': printf("Result: %.2lf\n", pow(a, b)); break;
+    case '+': printf("Result: %.2lf\n", a + b); break;
+    case '-': printf("Result: %.2lf\n", a - b); break;
+    case '*': printf("Result: %.2lf\n", a * b); break;
+    case '/': 
+        if (b != 0) printf("Result: %.2lf\n", a / b);
+        else printf("Cannot divide by zero.\n");
+        break;
+    case '^': printf("Result: %.2lf\n", pow(a, b)); break;
+    case '%': 
+        if ((int)b != 0) 
+            printf("Result: %d\n", (int)a % (int)b); 
+        else 
+            printf("Cannot modulo by zero.\n"); 
+        break;
+    default: printf("Invalid operator\n");
+}
 
-        default: printf("Invalid operator\n");
-    }
 
     return 0;
 }
