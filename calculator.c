9280dabaedfe1d1abaa1136bab8ba6b976159ff6
Arya <arya.kulkarni.345@gmail.com>
2025-04-08 22:06:13 +0530

    Initial commit with calculator and exponent operator

---
+++ calculator.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double a, b;
+    char op;
+
+    printf("Enter first number: ");
+    scanf("%lf", &a);
+    printf("Enter an operator (+, -, *, /, ^): ");
+    scanf(" %c", &op);
+    printf("Enter second number: ");
+    scanf("%lf", &b);
+
+    switch(op) {
+        case '+': printf("Result: %.2lf\n", a + b); break;
+        case '-': printf("Result: %.2lf\n", a - b); break;
+        case '*': printf("Result: %.2lf\n", a * b); break;
+        case '/': 
+            if (b != 0)
+                printf("Result: %.2lf\n", a / b);
+            else
+                printf("Cannot divide by zero.\n");
+            break;
+        case '^': printf("Result: %.2lf\n", pow(a, b)); break;
+        default: printf("Invalid operator\n");
+    }
+
+    return 0;
+}
+
